4d976e62fa828a0f4c2d2028a0cbcbe143c2de8b
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2026-04-27 16:33:44 +0530

    added headers in interview generation function

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -15,7 +15,9 @@ export const generateInterviewReport = async ({
   formData.append("selfDescription", selfDescription);
   formData.append("resume", resumeFile);
 
-  const response = await api.post("/api/interview", formData);
+  const response = await api.post("/api/interview", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 
   return response.data;
 };
